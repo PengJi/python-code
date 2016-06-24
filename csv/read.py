81343b3750a91f35fd1d08ed4446d89080700b4b
pengji <jipeng92@gmail.com>
2016-06-24 15:51:00 +0800

    modified:   read.py

--- csv/read.py
+++ csv/read.py
@@ -1,7 +1,17 @@
 import csv
-with open('test', 'rb') as f:
+
+lst=[]
+tup=('testtup')
+with open('demographic.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        print row[3].replace('`','').decode("gb2312")
-
+        #print row[0] + row[1].decode('UTF8') + row[2] + row[3].decode('UTF8') + row[4].decode('UTF8') + row[5].decode('UTF8') + row[6].decode('UTF8') + row[7].decode('UTF8') + row[8].decode('UTF8')
+		#tup = (row[0],row[1].decode('UTF8'),row[2],row[3].decode('UTF8'),row[4].decode('UTF8'),row[5].decode('UTF8'),row[6].decode('UTF8'),row[7].decode('UTF8'),row[8].decode('UTF8'))
+		tup = (row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
+		lst.append(tup)
 f.close()
+with open('demographic1.csv','wb') as f:
+	writer = csv.writer(f)
+	writer.writerows(lst)
+f.close()
+print lst
