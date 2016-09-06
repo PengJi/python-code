5d33b202887c79d15560d16c8f227179117f66e7
pengji <jipeng92@gmail.com>
2016-09-06 15:03:15 +0800

    monitoring remote hosts

---
+++ remote/ssh_remote.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python 
+
+# http://www.cnblogs.com/ma6174/archive/2012/05/25/2508378.html
+
+import paramiko
+import threading
+
+
+def ssh2(ip,username,passwd,cmd):
+
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip,22,username,passwd,timeout=5)
+        for m in cmd:
+            stdin, stdout, stderr = ssh.exec_command(m)
+            #stdin.write("Y")  # simple interaction, enter 'Y'
+            out = stdout.readlines()
+            # screen print
+            for o in out:
+                print o,
+        print '%s\tOK\n'%(ip)
+        ssh.close()
+    except:
+        print '%s\tError\n'%(ip)
+
+if __name__=='__main__':
+    cmd = ['cal','echo $PATH','hostname']  # commands list
+    username = "scidb"  # username
+    passwd = "scidb"    # password
+    threads = []   # multi-thread
+    print "Begin......"
+    for i in range(71,77):
+        ip = '192.168.100.'+str(i)
+        a=threading.Thread(target=ssh2,args=(ip,username,passwd,cmd))
+        a.start()
