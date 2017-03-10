108522aa9a491febe7784b634ebbe33c662c82f9
pengji <jipeng92@gmail.com>
2017-03-10 20:14:39 +0800

    multiprocessing event pipe queue pool

---
+++ paiallel/multiprocessing/pool1.py
@@ -0,0 +1,33 @@
+#coding: utf-8
+
+import multiprocessing
+import time
+
+'''
+使用线程池，非阻塞
+在利用Python进行系统管理的时候，特别是同时操作多个文件目录，或者远程控制多台主机，并行操作可以节约大量的时间。当被操作对象数目不大时，可以直接利用multiprocessing中的Process动态成生多个进程，十几个还好，但如果是上百个，上千个目标，手动的去限制进程数量却又太过繁琐，此时可以发挥进程池的功效。
+Pool可以提供指定数量的进程，供用户调用，当有新的请求提交到pool中时，如果池还没有满，那么就会创建一个新的进程用来执行该请求；但如果池中的进程数已经达到规定最大值，那么该请求就会等待，直到池中有进程结束，才会创建新的进程来它。
+'''
+
+def func(msg):
+    print "msg:", msg
+    time.sleep(3)
+    print "end"
+
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(processes = 3)
+    for i in xrange(4):
+        msg = "hello %d" %(i)
+        pool.apply_async(func, (msg, ))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
+
+    print "Mark~ Mark~ Mark~~~~~~~~~~~~~~~~~~~~~~"
+	# 关闭pool，使其不再接受新的任务
+    pool.close()
+	# 主进程阻塞，等待子进程的退出 
+    pool.join()   
+	#调用join之前，先调用close函数，否则会出错。执行完close后不会有新的进程加入到pool,join函数等待所有子进程结束
+    print "Sub-process(es) done."
+
+'''
+执行说明：创建一个进程池pool，并设定进程的数量为3，xrange(4)会相继产生四个对象[0, 1, 2, 4]，四个对象被提交到pool中，因pool指定进程数为3，所以0、1、2会直接送到进程中执行，当其中一个执行完事后才空出一个进程处理对象3，所以会出现输出“msg: hello 3”出现在"end"后。因为为非阻塞，主函数会自己执行自个的，不搭理进程的执行，所以运行完for循环后直接输出“mMsg: hark~ Mark~ Mark~~~~~~~~~~~~~~~~~~~~~~”，主程序在pool.join（）处等待各个进程的结束。
+'''
