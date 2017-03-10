2c0004febb55cb53fbff0d1cad19b117a0c620a7
pengji <jipeng92@gmail.com>
2017-03-10 17:31:15 +0800

    multiprocessing

---
+++ paiallel/multiprocessing/daemon2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import multiprocessing
+import time
+
+# 加daemon属性
+
+def worker(interval):
+    print("work start:{0}".format(time.ctime()));
+    time.sleep(interval)
+    print("work end:{0}".format(time.ctime()));
+
+if __name__ == "__main__":
+    p = multiprocessing.Process(target = worker, args = (3,))
+    p.daemon = True
+    p.start()
+    print "end!"
