4a3b57356ad20bf72dc823f5d7c306c87e19ba84
pengji <jipeng92@gmail.com>
2017-08-09 23:01:00 +0800

    add LHS.py

---
+++ arithmetic/LHS.py
@@ -0,0 +1,22 @@
+from pyDOE import *
+from numpy import *
+
+# MPL2
+def get_mpl2():
+    origin_mpl_2 = lhs(2,10)
+    
+
+# MPL3
+def get_mpl3():
+    origin_mpl_2 = lhs(3,10)
+    
+
+# MPL4
+def get_mpl4();
+    origin_mpl_2 = lhs(4,10)
+    
+
+# MPL5
+def get_mpl5():
+    origin_mpl_2 = lhs(5,10)
+    
