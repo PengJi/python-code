4aad121a50cbd167ed075e2b62f863ff906d5b87
pengji <jipeng92@gmail.com>
2017-08-14 09:33:27 +0800

    modified lhs.py

--- arithmetic/lhs.py
+++ arithmetic/lhs.py
@@ -4,19 +4,23 @@ from numpy import *
 # MPL2
 def get_mpl2():
     origin_mpl_2 = lhs(2,10)
-    
+    print origin_mpl_2
+    print ceil(origin_mpl_2*10)
 
 # MPL3
 def get_mpl3():
-    origin_mpl_2 = lhs(3,10)
-    
+    origin_mpl_3 = lhs(3,10)
+    print origin_mpl_3
 
 # MPL4
-def get_mpl4();
-    origin_mpl_2 = lhs(4,10)
-    
+def get_mpl4():
+    origin_mpl_4 = lhs(4,10)
+    print origin_mpl_4
 
 # MPL5
 def get_mpl5():
     origin_mpl_2 = lhs(5,10)
+    print origin_mpl_5
     
+if __name__ == '__main__':
+    get_mpl2()
