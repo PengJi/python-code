7109811e8ac9fdde71421d1440f4be7eec35aeb6
pengji <jipeng92@gmail.com>
2018-06-12 11:33:36 +0800

    add numpy

---
+++ packages/numpy/random/seed.py
@@ -0,0 +1,31 @@
+#coding: utf-8
+
+import numpy as np
+
+'''
+当设置相同的seed时，每次生成的随机数也相同，
+如果不设置seed，则每次生成的随机数也不同
+'''
+
+def test_seed():
+  a = np.random.rand(5)
+  print '第一次a: ', a
+
+  a = np.random.rand(5)
+  print '第二次a: ', a
+
+  np.random.seed(3)
+  b = np.random.rand(5)
+  print '第一次b: ', b
+
+  np.random.seed(3)
+  b = np.random.rand(5)
+  print '第二次b: ', b
+
+
+if __name__ == "__main__":
+  test_seed()
+
+
+
+
