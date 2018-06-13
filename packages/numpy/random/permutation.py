60aae136b390bf014512484868ed57dcc032980e
pengji <jipeng92@gmail.com>
2018-06-13 09:43:04 +0800

    add permutation.py rand.py seed.py

---
+++ packages/numpy/random/permutation.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+import numpy as np
+
+'''
+排列
+'''
+
+def test():
+  print 'test'
+
+if __name__ == "__main__":
+  test()
---
+++ packages/numpy/random/rand.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+import numpy as np
+
+'''
+简单随机数
+
+'''
