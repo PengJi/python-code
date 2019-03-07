976684dd67c4210501b4545de941308143b4c308
pengji <jipengpro@gmail.com>
2019-03-07 14:57:00 +0800

    class

--- pythonic/class/class_inheritance.py
+++ pythonic/class/class_inheritance.py
@@ -1,8 +1,8 @@
 #类的定义
 class a:
-    def __init__(self):#构造函数
-        self.m = 1#a类的成员变量
-    def add(self):#定义成员函数
+    def __init__(self):  #构造函数
+        self.m = 1  #a类的成员变量
+    def add(self):  #定义成员函数
         self.p=4
         print self.m+self.p
         
@@ -13,14 +13,14 @@ class b(a):
         self.n = 2
         self.tt()
     def sum(self,a=1,b=2):
-        """帮助文档信息""" #文档字符串的使用
+        """帮助文档信息"""   #文档字符串的使用
         print self.m+self.n
         self.__dd()
     def tt(self):
         self.m = 6
-    def  __dd(slef):# 私有函数
+    def  __dd(slef):  #私有函数
         print "dd"
-    def __ee__(self): #专有函数
+    def __ee__(self):  #专有函数
         print "ee"
         if 1==2:
             pass
