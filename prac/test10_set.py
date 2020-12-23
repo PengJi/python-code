6f5cacdfc6e4082e7f8aed2bcc793d328a0756d0
pengji <jipengpro@gmail.com>
2020-12-23 21:22:53 +0800

    delete files

--- prac/test10_set.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-
-#集合
-
-a = [1,2,3,4,5,6,7]
-b = set(a)
-print b
-
--- prac/testpk.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-#指定python的路径
-#!/usr/bin/python2.7
-
-#from testp import test4_class
-from testp.test4_class import b
-
-c = b()
-c.sum()
